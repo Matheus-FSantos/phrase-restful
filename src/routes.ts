967c53cddc0d48608304c751ab911f2c6631b7b3
matheus <matheus.fs.contato@gmail.com>
2023-07-31 16:26:21 -0300

    refator: adding the remaining methods

--- src/routes.ts
+++ src/routes.ts
@@ -7,7 +7,7 @@ const phraseController: PhraseController = new PhraseController();
 
 router.get("/phrases", phraseController.findAll );
 router.get("/phrases/:id", phraseController.findById );
-router.post("/prases", phraseController.save );
+router.post("/phrases", phraseController.save );
 router.put("/phrases/:id", phraseController.update );
 router.delete("/phrases/:id", phraseController.delete );
 
